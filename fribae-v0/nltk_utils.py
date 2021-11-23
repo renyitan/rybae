a00fd960cd599b751c577d0ed857a59bb11e1c40
Renyi Tan <tanrenyi@gmail.com>
2021-11-23 17:35:56 +0800

    fribae

--- fribae-v0/nltk_utils.py
+++ fribae-v0/nltk_utils.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
+import numpy as np
+
+# nltk.download('punkt')
 stemmer = PorterStemmer()
 
 
@@ -13,14 +15,16 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentece, all_words):
-    pass
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag = [     0,      1,      0,      1,   0,       0,     0, ]
+    """
+    tokenized_sentece = [stem(w) for w in tokenized_sentece]
+    # create an array of zeroes, same length as all_words[]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for index, w in enumerate(all_words):
+        if w in tokenized_sentece:
+            bag[index] = 1.0
 
-
-a = "how long does shipping take?"
-print(a)
-a = tokenize(a)
-print(a)
-
-words = ['organize', 'organizer', 'organizing']
-stemmed_word = [stem(word) for word in words]
-print(stemmed_word)
+    return bag
